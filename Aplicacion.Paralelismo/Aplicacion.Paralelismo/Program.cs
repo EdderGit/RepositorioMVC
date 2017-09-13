0cfb8225e43ccdc1e1adb07c1b7588f29fca92ab
edder.rodea@broxel.com <edder.rodea@broxel.com>
2017-09-13 11:59:44 -0500

    Se agrega proceso en paralelo

--- Aplicacion.Paralelismo/Aplicacion.Paralelismo/Program.cs
+++ Aplicacion.Paralelismo/Aplicacion.Paralelismo/Program.cs
@@ -10,6 +10,31 @@ namespace Aplicacion.Paralelismo
     {
         static void Main(string[] args)
         {
+            DateTime dtInicio = DateTime.Now;
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                int tmp = i;
+                Task t = Task.Factory.StartNew(() => MetodoProceso(tmp));
+            }
+
+            DateTime fin = DateTime.Now;
+            TimeSpan diff = fin - dtInicio;
+
+            Console.WriteLine("Inicio = {0} - Fin = {1} - Diferencias = {2}:{3}:{4}.{5}", 
+                                                                                        dtInicio.ToString("yyyy/MM/dd HH:mm:ss.ffffff") , 
+                                                                                        fin.ToString("yyyy/MM/dd HH:mm:ss.ffffff"),diff.Hours, diff.Minutes,diff.Seconds,diff.Milliseconds );
+
+            Console.ReadKey();
+        }
+
+
+        static void MetodoProceso( int i )
+        {
+            for (int j = 0; j < 100000; j++)
+            {
+                double d = 45345/6546*7989/0.2254;
+            }
         }
     }
 }
