8f3fd65293e03f4036a130c63630b3a32e694229
edder.rodea@broxel.com <edder.rodea@broxel.com>
2017-09-13 13:26:34 -0500

    Se agrega clase empleado

---
+++ Aplicacion.Paralelismo/Aplicacion.Paralelismo/Empleado.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Paralelismo
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string Nombre  { get; set; }
+        public int Edad       { get; set; }
+
+
+
+
+        public static List<Empleado> ObtenerEmpleado(int n)
+        {
+            Random r = new Random( Guid.NewGuid().GetHashCode() );
+
+            List<Empleado> empleados = new List<Empleado>();
+
+            for (int i = 0; i < n; i++)
+            {
+                empleados.Add(new Empleado
+                {
+                    IdEmpleado = i,
+                    Nombre = string.Format("Empleado {0}" , i),
+                    Edad = r.Next(20 , 80)
+                });
+            }
+            return empleados;
+        }
+    }
+}
--- Aplicacion.Paralelismo/Aplicacion.Paralelismo/Program.cs
+++ Aplicacion.Paralelismo/Aplicacion.Paralelismo/Program.cs
@@ -8,26 +8,21 @@ namespace Aplicacion.Paralelismo
 {
     class Program
     {
+        private static Dictionary<int, Empleado> _dictionary;
+
         static void Main(string[] args)
         {
 
-            MetodoTarea1();
+            //MetodoTarea1();
+            //MetodoTarea2()
+              
+            
+            _dictionary = new Dictionary<int, Empleado>();
+            _dictionary = Empleado.ObtenerEmpleado(100000).ToDictionary(p => p.IdEmpleado);
 
-            DateTime dtInicio = DateTime.Now;
+            IniciarConcurrencia();
 
-            for (int i = 0; i < 100000; i++)
-            {
-                int tmp = i;
-                Task t = Task.Factory.StartNew(() => MetodoProceso(tmp));
-            }
-
-            DateTime fin = DateTime.Now;
-            TimeSpan diff = fin - dtInicio;
-
-            Console.WriteLine("Inicio = {0} - Fin = {1} - Diferencias = {2}:{3}:{4}.{5}", 
-                                                                                        dtInicio.ToString("yyyy/MM/dd HH:mm:ss.ffffff") , 
-                                                                                        fin.ToString("yyyy/MM/dd HH:mm:ss.ffffff"),diff.Hours, diff.Minutes,diff.Seconds,diff.Milliseconds );
-
+            Console.WriteLine("Termino concurrencia");
             Console.ReadKey();
         }
 
@@ -51,5 +46,46 @@ namespace Aplicacion.Paralelismo
 
             Console.ReadKey();
         }
+
+        static void MetodoTarea2()
+        {
+                    DateTime dtInicio = DateTime.Now;
+
+            for (int i = 0; i < 100000; i++)
+            {
+                int tmp = i;
+                Task t = Task.Factory.StartNew(() => MetodoProceso(tmp));
+            }
+
+            DateTime fin = DateTime.Now;
+            TimeSpan diff = fin - dtInicio;
+
+            Console.WriteLine("Inicio = {0} - Fin = {1} - Diferencias = {2}:{3}:{4}.{5}",
+                                                                                        dtInicio.ToString("yyyy/MM/dd HH:mm:ss.ffffff"), fin.ToString("yyyy/MM/dd HH:mm:ss.ffffff"), diff.Hours, diff.Minutes, diff.Seconds, diff.Milliseconds);
+                            
+        }
+
+        static void EliminarItem(int id)
+        {
+            if (_dictionary.ContainsKey(id) && _dictionary[id].Edad < 30) _dictionary.Remove(id);
+        }
+
+        static void ActualizarItem(int id)
+        {
+            if (_dictionary.ContainsKey(id)) _dictionary[id].Edad++;
+        }
+
+        static void IniciarConcurrencia()
+        {
+            for (int i = 0; i < 7000; i++)
+            {
+                Random r = new Random( Guid.NewGuid().GetHashCode());
+                int id = r.Next(1, _dictionary.Count);
+                Task.Factory.StartNew( () => EliminarItem(id));
+                Task.Factory.StartNew( () => ActualizarItem(id));
+                
+            }
+        }
+
     }
 }
